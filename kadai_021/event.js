032b6c376d16fa22a5c60b92fb3e0b8f80b6e18c
u8nkgw <un44448699@gmail.com>
2023-11-30 21:54:00 +0900

    課題021追加

---
+++ kadai_021/event.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+
+  },2000);
+
+  const text =document.getElementById('text');
+
+  
+  
+
+})
